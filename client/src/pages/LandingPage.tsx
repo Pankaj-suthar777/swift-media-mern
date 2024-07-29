3240b841510b66d36552eb3ceef6f9176cbf2fda
pankaj suthar <pankajss0070@gmail.com>
2024-07-29 18:30:41 +0530

    landing page

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,4 +1,5 @@
-import { BackgroundBeams } from "@/components/layout/background-beams";
+import { GridSmallBackgroundDemo } from "@/components/landing/GridSmallBackgroundDemo";
+import { ScrollCards } from "@/components/landing/ScrollCards";
 import Header from "@/components/layout/Header";
 import { Spotlight } from "@/components/layout/spotlight";
 import { useAppSelector } from "@/store/hooks";
@@ -50,10 +51,14 @@ const LandingPage = () => {
           </div>
         </div>
       </div>
+      <div className="">
+        <ScrollCards />
+      </div>
+
       {/* second part here*/}
-      <div className="h-[40rem] w-full bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+      <div className="w-full bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
         <div className="max-w-2xl mx-auto p-4"></div>
-        <BackgroundBeams />
+        <GridSmallBackgroundDemo />
       </div>
     </>
   );
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -75,6 +75,13 @@
 //   },
 //   plugins: [require("tailwindcss-animate")],
 // };
+const svgToDataUri = require("mini-svg-data-uri");
+
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
@@ -135,6 +142,11 @@ module.exports = {
         sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
         "accordion-down": {
           from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
@@ -159,8 +171,46 @@ module.exports = {
         "accordion-up": "accordion-up 0.2s ease-out",
 
         spotlight: "spotlight 2s ease .75s 1 forwards",
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwindcss-animate"),
+    addVariablesForColors,
+    function ({ matchUtilities, theme }: any) {
+      matchUtilities(
+        {
+          "bg-grid": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+          "bg-grid-small": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+          "bg-dot": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+      );
+    },
+  ],
 };
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
